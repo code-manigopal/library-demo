f4adee7166e2658340996c849b502a87963e22c2
Kanaga Manikandan Gopal <manikandangopal97@gmail.com>
2019-11-21 12:18:41 +0530

    Update json_parser.groovy

--- vars/json_parser.groovy
+++ vars/json_parser.groovy
@@ -1,3 +1,4 @@
+import groovy.json.JsonSlurperClassic
 //jsonParser - used to parse the JSON and turn it to map
 Object jsonParser(String response){
     def slurper = new groovy.json.JsonSlurperClassic()
